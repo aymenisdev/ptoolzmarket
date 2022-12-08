52c79a4177050e82547189205cc8c01542175721
Aymen Alsinary <118968603+aymenisdev@users.noreply.github.com>
2022-12-08 18:07:52 +0200

    Delete lib directory

--- lib/backend/conest.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:sales/backend/fun.dart';
-
-final materialPrimaryColor =
-    buildMaterialColor(const Color.fromRGBO(253, 85, 161, 1));
-const primaryColor = Color.fromRGBO(253, 85, 161, 1);
